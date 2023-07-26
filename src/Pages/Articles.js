1ae8d760aa2444759b318106a99c0badd7640e67
PSC <nevergunnasuckonthesenuts68@gmail.com>
2023-07-26 15:02:55 +0700

    fixed a little bug with the navbar, added aesthetics to articles page

--- src/Pages/Articles.js
+++ src/Pages/Articles.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Articles.css'
+import './Articles.css';
 
 
 export default function Archive(){
@@ -9,7 +9,37 @@ export default function Archive(){
 
             <div className="filter-box">
                 <div className="filter-caption">FILTER BY</div>
+                <div className="filter-area">
+                    <div className="fields">
+                        <h2>Research Area</h2>
+                        <div className="natural-sciences" >
+                            <FilterItem name="Natural Sciences" style={{fontWeight:"bold", fontSize: "1em"}} />
+                            <ul className="sublist">
+                                <FilterItem name="Life Sciences" />
+                                <FilterItem name="Physical Sciences" />
+                                <FilterItem name="Earth Sciences" />
+                                <FilterItem name="Medical and Health" />
+                                <FilterItem name="Mathematics" />
+                                <FilterItem name="Formal Sciences" />
+                            </ul>
+
+                        </div>
+                        <div className="social-sciences">
+                            <FilterItem name="Social Sciences" style={{fontWeight:"bold", fontSize: "1em"}} />
+                                <ul className="sublist">
+                                    <FilterItem name="Social Studies" />
+                                    <FilterItem name="Humanities" />
+                                
+                                </ul>
+                        </div>
+                    </div>
+                    <div className="status"></div>
+                    <div className="date-published"></div>
+                    <div className="language"></div>
+                </div>
             </div>
+
+            
             <div className="search-box">
                 <div className="search-bar"></div>
                 <div className="search-results"></div>
@@ -18,3 +48,13 @@ export default function Archive(){
         </>
     )
 }
+
+function FilterItem(props){
+    return (
+        <li className="no-list-style sub-list-items" style={props.style}>
+            <input type="checkbox"/> 
+            <span> {props.name}</span>
+        </li>
+    )
+}
+
