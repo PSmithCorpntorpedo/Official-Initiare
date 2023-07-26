adc241cf7d985968e247ae0d7e68c5509cbe714b
dbknguyen <doanbakhanhnguyen7@gmail.com>
2023-07-27 00:00:59 +0700

    Fixed various issues with Navbar and About page.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,54 +1,56 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './Navbar.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faBars, faTimes} from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 
 
-function Navbar(){
-    const bars = <FontAwesomeIcon icon = {faBars}/>
-    const x = <FontAwesomeIcon icon = {faTimes}/>
+function Navbar() {
+    const bars = <FontAwesomeIcon icon={faBars} />
+    const x = <FontAwesomeIcon icon={faTimes} />
 
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
-    return(
-        <>
+
+    return (
+        <div className="nav-wrap">
             <nav className="navbar">
                 <div className="logo-container">
                     <Link to="/" >
-                    <img src="/Images/initiare-logo-final-centered.png" className="logo"/> 
+                        <img src="/Images/initiare-logo-final-centered.png" className="logo" />
                     </Link>
                 </div>
                 <div className="menu-container">
                     <div className="menu-icons" onClick={handleClick}>
-                    {click ? x : bars}
+                        {click ? x : bars}
                     </div>
                 </div>
-                
-                
+
+
             </nav>
             <ul className={click ? "nav-menu active" : "nav-menu inactive"}>
-                    <DropdownItem title="Home" link ="/" closeMenu={handleClick}/>
-                    <DropdownItem title="About" link ="/about" closeMenu={handleClick}/>
-                    <DropdownItem title="Archive" link ="/archive" closeMenu={handleClick}/>
-                    <DropdownItem title="Upload" link ="/upload" closeMenu={handleClick}/>
-                    <DropdownItem title="Blog" link ="/blog" closeMenu={handleClick}/>
-                    <DropdownItem title="InitiaRe x Scholar Journal" link ="/ixsjournal" closeMenu={handleClick}/>
-                </ul>
-        </>
+                <DropdownItem title="Home" link="/" closeMenu={handleClick} />
+                <DropdownItem title="About" link="/about" closeMenu={handleClick} />
+                <DropdownItem title="Archive" link="/archive" closeMenu={handleClick} />
+                <DropdownItem title="Upload" link="/upload" closeMenu={handleClick} />
+                <DropdownItem title="Blog" link="/blog" closeMenu={handleClick} />
+                <DropdownItem title="InitiaRe x Scholar Journal" link="/ixsjournal" closeMenu={handleClick} />
+            </ul>
+
+        </div>
     );
 };
 
-function DropdownItem(props){
-    return(
+function DropdownItem(props) {
+    return (
         <>
             <li className="nav-pages" onClick={props.closeMenu}>
                 <Link to={props.link} className="nav-links">
-                {props.title}
+                    {props.title}
                 </Link>
             </li>
-            
+
         </>
     );
 }
