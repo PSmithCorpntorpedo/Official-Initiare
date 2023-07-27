7a4b7196cb337cf6f44512ae54c6c9f741cebfd8
PSC <nevergunnasuckonthesenuts68@gmail.com>
2023-07-27 18:32:53 +0700

    fixed multiple bugs related to the articles page, whilst adding additional responsiveness for mobile devices

--- src/Pages/Articles.js
+++ src/Pages/Articles.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import './Articles.css';
+import {useState} from 'react';
 
 
-export default function Archive(){
+export default function Articles(){
     return(
-        <>
         <div className="page-wrapper">
+            <FilterBox />          
+            <SearchBox />
+        </div>
+    )
+}
 
-            <div className="filter-box">
-                <div className="filter-caption">FILTER BY</div>
-                <div className="filter-area">
-                    <div className="fields">
-                        <h2>Research Area</h2>
-                        <div className="natural-sciences" >
-                            <FilterItem name="Natural Sciences" style={{fontWeight:"700", fontSize: "1.25em"}} />
+function FilterItem(props){
+    return (
+        <li className="no-list-style sub-list-items" style={props.style} >
+            <label for={props.name}>
+                <input type="checkbox" id={props.name}/> 
+                {props.name}
+            </label>
+        </li>
+    )
+}
+
+function FilterBox(){
+   return(
+   <div className="filter-box">
+        <div className="filter-caption">FILTER BY</div>
+            <div className="filter-area">
+                <div className="fields">
+                    <h2>Research Area</h2>
+                    <div className="natural-sciences" >
+                    <FilterItem name="Natural Sciences" style={{fontWeight:"700", fontSize: "1.2em"}} />
                             <ul className="sublist1">
                                 <FilterItem name="Life Sciences" />
                                 <FilterItem name="Physical Sciences" />
@@ -23,14 +41,13 @@ export default function Archive(){
                                 <FilterItem name="Formal Sciences" />
                             </ul>
 
-                        </div>
-                        <div className="social-sciences">
-                            <FilterItem name="Social Sciences" style={{fontWeight:"700", fontSize: "1.25em"}} />
-                                <ul className="sublist1">
-                                    <FilterItem name="Social Studies" />
-                                    <FilterItem name="Humanities" />
-                                
-                                </ul>
+                    </div>
+                    <div className="social-sciences">
+                    <FilterItem name="Social Sciences" style={{fontWeight:"700", fontSize: "1.2em"}} />
+                            <ul className="sublist1">
+                                <FilterItem name="Social Studies" />
+                                <FilterItem name="Humanities" />
+                            </ul>
                         </div>
                     </div>
                     <div className="header status">
@@ -49,28 +66,26 @@ export default function Archive(){
                             <FilterItem name="2022" />
                         </ul>
                     </div>
-                    <div className="header language"></div>
+                    <div className="header language">
+                        <h2>Language</h2>
+                        <ul className="sublist2">
+                            <FilterItem name="English" />
+                            <FilterItem name="Vietnamese" />
+                        </ul>
+                    </div>
                 </div>
-            </div>
+    </div>
+    )
+}
 
-            
-            <div className="search-box">
-                <div className="search-bar"></div>
+function SearchBox(){   
+    const [search, setSearch] = useState('');
+    return(
+        <div className="search-box">
+                <div className="search-bar">
+                    <input type="text"/>
+                </div>
                 <div className="search-results"></div>
-            </div> 
-        </div>
-        </>
+        </div> 
     )
 }
-
-function FilterItem(props){
-    return (
-        <li className="no-list-style sub-list-items" style={props.style} >
-            <label for={props.name}>
-                <input type="checkbox" id={props.name}/> 
-                {props.name}
-            </label>
-        </li>
-    )
-}
-
