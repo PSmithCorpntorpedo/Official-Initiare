01fb8609160dc6127c93483d4997da79a018581c
dbknguyen <doanbakhanhnguyen7@gmail.com>
2023-07-30 15:58:26 +0700

    Switched to using CSS modules

---
+++ src/Pages/jsx/Home.jsx
@@ -0,0 +1,30 @@
+import homecss from './../css/home.module.css';
+import headerVideo from './../assets/pexels-pixabay-855204-1920x1080-30fps.mp4';
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className={homecss['home']}>
+            <div className={homecss['home-header']}>
+                <video className={homecss['header-video']} src={headerVideo} muted autoPlay loop></video>
+                <div className={homecss['main-text-wrap']}>
+                    <h1 className={homecss['main-text']}>InitiaRe</h1>
+                </div>
+                <div className={homecss['clock-wrap-wrap']}>
+                    <div className={homecss['clock-wrap']}>
+                        <div className={homecss['hour-mark']}></div>
+                        <div className={homecss['hour-mark']}></div>
+                        <div className={homecss['hour-mark']}></div>
+                        <div className={homecss['hour-mark']}></div>
+                        <div className={homecss['hour-mark']}></div>
+                        <div className={homecss['hour-mark']}></div>
+                        <div className={homecss['long-hand']}></div>
+                        <div className={homecss['short-hand']}></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
