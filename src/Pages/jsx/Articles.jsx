8a27b19af6359c292e620c306f0609f456811b13
PSC <nevergunnasuckonthesenuts68@gmail.com>
2023-07-30 11:46:03 +0700

    successfully implemented mock data fetching as well pagination with functioning traversal

--- src/Pages/jsx/Articles.jsx
+++ src/Pages/jsx/Articles.jsx
@@ -97,21 +97,73 @@ function SearchBox(){
 function Paginate(){
     const prev = <FontAwesomeIcon icon={faArrowLeft}/>
     const next = <FontAwesomeIcon icon={faArrowRight}/>
+    let limit = 12
 
-    const handlePageClick = (page) => {
-        console.log(page.selected + 1)
+    const [items, setItems] = useState([])
+    const [pageCount, setPageCount] = useState(0)
+
+    useEffect(() => {
+        const getComments = async () => {
+            const res = await fetch(`https://jsonplaceholder.typicode.com/comments?_page=1&_limit=12`)
+            const data = await res.json()
+            const total = res.headers.get('x-total-count')
+            setPageCount(Math.ceil(total/12)) 
+            setItems(data)
+        }
+        getComments()
+        /*this is essentially a one time use method that loads everytime the page reloads*/
+    }, [])
+
+
+    const fetchPageComments = async (page) => {
+         
+        const res = await fetch(`https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=${limit}`)
+        const data = await res.json()
+        return data
+    }
+
+    const handlePageClick = async (input) => {
+        const page = input.selected + 1;
+        const pageServer = await fetchPageComments(page)
+        setItems(pageServer)
     }
     return(
         <div className="search-results">
+            <div className="row m-2">
+        {items.map((item) => {
+          return (
+            <div key={item.id} className="col-sm-6 col-md-4 v my-2">
+              <div className="shadow-sm w-100" style={{ minHeight: 225}}>
+                <div className="card-body" style={{ zIndex:'1'}}>
+                  <h5 className="card-title text-center h2" style={{ zIndex:'1'}}>Id :{item.id} </h5>
+                  <h6 className="card-subtitle mb-2 text-muted text-center">
+                    {item.email}
+                  </h6>
+                  <p className="card-text" style={{ zIndex:'1'}}>{item.body}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
             <ReactPaginate 
             previousLabel = {prev}
             nextLabel = {next} 
             breakLabel = {'...'}
-            pageCount = {10}
+            pageCount = {pageCount}
             marginPagesDisplayed={1}
             onPageChange={handlePageClick}
-            containerClassName=''
+            containerClassName={'pagination justify-content-center'}
+            pageClassName={'page-item'}
+            pageLinkClassName={'page-link'}
+            previousClassName={'page-item'}
+            previousLinkClassName={'page-link'}
+            nextClassName={'page-item'}
+            nextLinkClassName={'page-link'}
+            breakClassName={'page-item'}
+            breakLinkClassName={'page-link'}
+            activeClassName={'active'}
             />
+        </div>
         </div>
     )
 }
