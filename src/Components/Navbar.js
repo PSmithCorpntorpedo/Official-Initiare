520505abd316736d4ee2967c018a8b933f108408
dbknguyen <doanbakhanhnguyen7@gmail.com>
2023-08-05 17:25:37 +0700

    Moved nav icon to the left, added site name to navbar, troubleshooted problems with navbar on different devices.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -16,26 +16,27 @@ function Navbar() {
     const [show, setShow] = useState(true);
     const [lastScrollY, setLastScrollY] = useState(0);
 
-    const controlNavbar = () => {
-        let navGutter = document.getElementById('nav-gutter');
-        if (typeof window !== 'undefined') {
-            if (window.scrollY > lastScrollY && click === false && window.scrollY > (navGutter.offsetTop + navGutter.offsetHeight)) { 
-                /*  if:  1. the user scrolls down
-                        2. the dropdown isn't active
-                        3. the user has scrolled past where the navbar lives at the top of the screen
-                    then: hide the navbar.  */ 
-                setShow(false);
-            } else { // if scroll up show the navbar
-                setShow(true);
-            }
-
-            // remember current page location to use in the next move
-            setLastScrollY(window.scrollY);
-        }
-    };
-
     // event listener to get scroll position
     useEffect(() => {
+        // moved controlNavbar inside of useEffect to prevent warning
+        const controlNavbar = () => {
+            let navGutter = document.getElementById('nav-gutter');
+            if (typeof window !== 'undefined') {
+                if (window.scrollY > lastScrollY && click === false && window.scrollY > (navGutter.offsetTop + navGutter.offsetHeight)) { 
+                    /*  if: 1. the user scrolls down
+                            2. the dropdown isn't active
+                            3. the user has scrolled past where the navbar lives at the top of the screen
+                        then: hide the navbar.  */ 
+                    setShow(false);
+                } else { // if scroll up show the navbar
+                    setShow(true);
+                }
+    
+                // remember current page location to use in the next move
+                setLastScrollY(window.scrollY);
+            }
+        };
+
         // console.log(lastScrollY);
         if (typeof window !== 'undefined') {
             window.addEventListener('scroll', controlNavbar);
@@ -45,25 +46,29 @@ function Navbar() {
                 window.removeEventListener('scroll', controlNavbar);
             };
         }
-    }, [lastScrollY]);
-
+    }, [lastScrollY, click]);
 
     return (
         <div>
             {/* the navbar itself*/}
             <div className={`${navbarcss['nav-wrap']} ${show ? navbarcss.shown : navbarcss.hidden}`}>
                 <nav className={navbarcss['navbar']}>
-                    <div className={navbarcss['logo-container']}>
-                        <Link to="/" >
+                    <div className={navbarcss['left-container']}>
+                        <div className={navbarcss['menu-container']}>
+                            <div className={navbarcss['menu-icons']} onClick={handleClick}>
+                                {click ? x : bars}
+                            </div>
+                        </div>
+                        <Link to='/' className={navbarcss['logo-container']}>
                             <img src="/Images/initiare-logo-final-centered.png" alt='logo' className={navbarcss['logo']} />
                         </Link>
-                    </div>
-                    <div className={navbarcss['menu-container']}>
-                        <div className={navbarcss['menu-icons']} onClick={handleClick}>
-                            {click ? x : bars}
+                        <div className={navbarcss['site-name-container']}>
+                            <div className={navbarcss['site-name']}>InitiaRe</div>
                         </div>
                     </div>
-
+                    <div className={navbarcss['right-container']}>
+                        
+                    </div>             
 
                 </nav>
                 <ul className={`${navbarcss['nav-menu']} ${click ? navbarcss['active'] : navbarcss['inactive']}`}>
