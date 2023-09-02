c82c0827d0ec7fe4b41ada070ab85793b9975bd1
dbknguyen <doanbakhanhnguyen7@gmail.com>
2023-09-02 20:50:05 +0700

    Finished first upload page. Added placeholder second upload page.

---
+++ src/Pages/jsx/ConfirmUpload.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import cfucss from './../css/confirmupload.module.css';
+
+function ConfirmUpload() {
+    return(
+        <h1>Confirm upload page</h1>
+    )
+}
+
+export default ConfirmUpload
