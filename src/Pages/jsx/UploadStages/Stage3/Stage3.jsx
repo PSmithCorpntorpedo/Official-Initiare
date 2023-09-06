c800c8770598aa87c0365206266f52e21c88ccbf
PSC <nevergunnasuckonthesenuts68@gmail.com>
2023-09-06 20:55:04 +0700

    Added stage 4 and option highlighting

--- src/Pages/jsx/UploadStages/Stage3/Stage3.jsx
+++ src/Pages/jsx/UploadStages/Stage3/Stage3.jsx
@@ -3,50 +3,55 @@ import u3css from "./Stage3.module.css";
 import usePrimaryCategories from "../../../../Hooks/usePrimaryCategories";
 
 export default function Stage3JSX() {
-  
+  const [chosenCat, setChosenCat] = useState("");
+  const HandleChoose = (e) => {
+    setChosenCat(e.target.value);
+  }
   return (
     <div className={`${u3css[`panel`]}`}>
       <p className={`${u3css[`primary-question`]}`}>
         What is the PRIMARY area of study of your paper?
         <br />
-        <span>(no more than one area)</span>
+        <span>(Please choose one only)</span>
       </p>
       <div className={`${u3css[`choices`]}`}>
         <div className={`${u3css[`option-wrapper`]}`}>
           <p className={`${u3css[`option-header`]}`}>Natural Sciences</p>
-          <Options name="Life Sciences" />
-          <Options name="Physical Sciences" />
-          <Options name="Earth Sciences" />
-          <Options name="Medical and Health" />
-          <Options name="Mathematics" />
-          <Options name="Formal Sciences" />
+          <Options name="Life Sciences" setChosenCat={HandleChoose} chosenCat={chosenCat}/>
+          <Options name="Physical Sciences" setChosenCat={HandleChoose} chosenCat={chosenCat}/>
+          <Options name="Earth Sciences" setChosenCat={HandleChoose} chosenCat={chosenCat}/>
+          <Options name="Medical and Health" setChosenCat={HandleChoose} chosenCat={chosenCat}/>
+          <Options name="Mathematics" setChosenCat={HandleChoose} chosenCat={chosenCat}/>
+          <Options name="Formal Sciences" setChosenCat={HandleChoose} chosenCat={chosenCat}/>
         </div>
 
         <div className={`${u3css[`option-wrapper`]}`}>
           <p className={`${u3css[`option-header`]}`}>Social Sciences</p>
-          <Options name="Social Studies" />
-          <Options name="Humanities" />
+          <Options name="Social Studies" setChosenCat={HandleChoose} chosenCat={chosenCat}/>
+          <Options name="Humanities" setChosenCat={HandleChoose} chosenCat={chosenCat}/>
         </div>
       </div>
     </div>
   );
 }
 
-const Options = ({ name }) => {
+const Options = ({ name, setChosenCat, chosenCat }) => {
   const {setPCategory} = usePrimaryCategories();
+  
   return (
-    <div className={`${u3css[`option`]}`}>
-      <input
+    <div className={`${u3css[`option`]} ${chosenCat === "Primary" + name ? u3css[`chosen`] : u3css[`not-chosen`]} `}>
+      <input 
         type="radio"
-        id={name}
+        id={"Primary" + name}
         name="option"
         className={`${u3css[`radio-button`]}`}
-        value={name}
+        value={"Primary" + name}
         onChange={(e) => {
           setPCategory(e.target.value);
+          setChosenCat(e)
         }}
       />
-      <label for={name} className={`${u3css[`option-label`]}`}>
+      <label for={"Primary" + name} className={`${u3css[`primary-label`]}`}>
         <span>{name}</span>
       </label>
     </div>
