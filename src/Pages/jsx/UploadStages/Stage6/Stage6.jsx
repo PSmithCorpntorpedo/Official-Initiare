9b114a3aea6f695cd137b49dd3d0a9ff4c0dc603
PSC <nevergunnasuckonthesenuts68@gmail.com>
2023-09-12 20:23:20 +0700

    Remake colors in previous stages, created early version of last stage

--- src/Pages/jsx/UploadStages/Stage6/Stage6.jsx
+++ src/Pages/jsx/UploadStages/Stage6/Stage6.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import u6css from "./Stage6.module.css";
 import useFile from "../../../../Hooks/useFile";
 import useAuth from "../../../../Hooks/useAuth";
 import useCategories from "../../../../Hooks/useCategories";
@@ -7,8 +8,8 @@ import usePaperType from "../../../../Hooks/usePaperType";
 
 export default function Stage6JSX() {
   const { category } = useCategories();
-  const {paperType} = usePaperType();
-  const {subCategory} = useSubCategories();
+  const { paperType } = usePaperType();
+  const { subCategory } = useSubCategories();
   const { file } = useFile();
   const { auth } = useAuth();
 
@@ -40,17 +41,77 @@ export default function Stage6JSX() {
   };
 
   return (
-    <>
-      <button onClick={handleClick}>Upload File</button>
-      <button
-        onClick={() => {
-          console.log(category);
-          console.log(subCategory);
-          console.log(paperType)
-        }}
-      >
-        Check if all categories works until stage 6
-      </button>
-    </>
+    <div className={`${u6css[`panel`]}`}>
+      <p>
+        Before submitting, ensure your review paper satisfy all of the following
+        criteria.
+      </p>
+      <div className={`${u6css[`tickboxes`]}`}>
+        <Options
+          name="Title"
+          content="Clear, concise, and reflect the main focus of the review."
+        />
+        <Options
+          name="Abstract"
+          content=" A brief summary of the entire paper. Includes the research question, methodology, key findings, and conclusions."
+        />
+        <Options
+          name="Introduction"
+          content="
+        -Presents the background and context of the study. 
+         -Clearly states the research problem or question.  
+         -Reviews relevant literature and previous research. 
+          -Outlines the objectives and goals of the study. "
+        />
+        <Options
+          name="Keywords"
+          content="Words that define the research topic, important words/concepts found in your research question or thesis."
+        />
+        <Options
+          name="Keywords"
+          content="Words that define the research topic, important words/concepts found in your research question or thesis."
+        />
+        
+      </div>
+    </div>
   );
 }
+
+const Options = ({ name, content }) => {
+  const [isChosen, setIsChosen] = useState(false);
+  function handleChange(e) {
+    setIsChosen(!isChosen);
+    let { checked } = e.target;
+
+    if (checked) {
+    } else if (!checked) {
+    }
+  }
+
+  return (
+    <div
+      className={`${u6css[`option`]} ${
+        isChosen ? u6css[`chosen`] : u6css[`not-chosen`]
+      }`}
+    >
+      <input
+        type="checkbox"
+        id={name}
+        name="option"
+        className={`${u6css[`checkbox`]}`}
+        value={name}
+        onChange={handleChange}
+      />
+      <div
+        className={`${u6css[`before-primary-label`]} ${
+          isChosen ? u6css[`chosen-label`] : u6css[`not-chosen`]
+        }`}
+      ></div>
+      <label for={name} className={`${u6css[`label`]}`}>
+        <span>
+          {name}: {content}
+        </span>
+      </label>
+    </div>
+  );
+};
